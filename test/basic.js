878aa378b472225e4604f530cc94d39ea497f4f4
up9cloud <up9cloud.net@gmail.com>
2017-01-10 17:09:43 +0800

    init commit

---
+++ test/basic.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const chai = require('chai');
+chai.use(require('../src/index.js'));
+const expect = chai.expect;
+
+describe('basic test', function () {
+    it('apple should match fruit schema.', function () {
+        let apple = {
+            name: 'foo',
+            color: ['red', 'green', 'yellow'],
+            value: 10
+        };
+        let schema = {
+            title: 'fruit schema v0.1',
+            type: 'object',
+            required: ['name', 'color', 'value'],
+            properties: {
+                name: {
+                    type: 'string',
+                    minLength: 3
+                },
+                color: {
+                    type: 'array',
+                    minItems: 1,
+                    uniqueItems: true,
+                    items: {
+                        type: 'string'
+                    }
+                },
+                value: {
+                    type: 'integer',
+                    minimum: 5
+                }
+            }
+        }
+        expect(apple).to.be.jsonSchema(schema);
+    });
+    it('should print error message when not match schema.', function () {
+        let apple = {
+            value: 1.1,
+        };
+        let schema = {
+            type: 'object',
+            required: ['value'],
+            properties: {
+                value: {
+                    type: 'integer',
+                },
+            }
+        }
+        try {
+            expect(apple).to.be.jsonSchema(schema);
+        } catch (err) {
+            expect(err instanceof chai.AssertionError).to.be.true;
+            expect(err.message).to.be.equal(`expected value not match the json-schema
+[
+  {
+    "keyword": "type",
+    "dataPath": ".value",
+    "schemaPath": "#/properties/value/type",
+    "params": {
+      "type": "integer"
+    },
+    "message": "should be integer"
+  }
+]`);
+        }
+    });
+});

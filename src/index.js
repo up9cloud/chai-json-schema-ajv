1a6e24515019b88b04486215f5c067747f7658c7
up9cloud <8325632+up9cloud@users.noreply.github.com>
2021-04-19 00:11:49 -0700

    fix: don't check configs.ajv, trust them :)
    People may pass the non-default ajv instance, we can't simply check options.ajv is default Ajv
    instance, see https://github.com/up9cloud/chai-json-schema-ajv/issues/68
    
    fix #68

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const Ajv = require('ajv').default ? require('ajv').default : require('ajv')// ? `v7` : `<= v6`
+const Ajv = require('ajv').default ? require('ajv').default : require('ajv')// ? `>= v7` : `<= v6`
 
 function _createPlugin (chai, utils, options) {
   let ajv
-  if (options && options.ajv instanceof Ajv) {
+  if (options && options.ajv) {
     ajv = options.ajv
   } else {
     ajv = new Ajv(options)
@@ -29,7 +29,7 @@ function _createPlugin (chai, utils, options) {
     let detail
     if (
       verbose ||
-      (ajv.opts && ajv.opts.verbose) || // v7
+      (ajv.opts && ajv.opts.verbose) || // >= v7
       (ajv._opts && ajv._opts.verbose) // <= v6
     ) {
       placeholder = '#{this}'
@@ -57,7 +57,7 @@ function _createPlugin (chai, utils, options) {
     let detail
     if (
       verbose ||
-      (ajv.opts && ajv.opts.verbose) || // v7
+      (ajv.opts && ajv.opts.verbose) || // >= v7
       (ajv._opts && ajv._opts.verbose) // <= v6
     ) {
       placeholder = '#{this}'
